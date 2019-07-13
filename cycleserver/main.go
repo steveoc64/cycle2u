bc21225766e48d751e12abb55f32e75d7f2951b8
steveoc <steveoc64@gmail.com>
2019-07-14 02:30:17 +1000

    hacking

--- cycleserver/main.go
+++ cycleserver/main.go
@@ -1,13 +1,154 @@
 package main
 
 import (
+	"fmt"
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"log"
 	"net/mail"
+	"net/smtp"
 	"time"
 
 	gomail "gopkg.in/jpoehls/gophermail.v0"
 )
 
 func main() {
+	log.Println("Connecting to server...")
+
+	// Connect to server
+	c, err := client.DialTLS("mail.cycle2u.com.au:993", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected")
+
+	// Don't forget to logout
+	defer c.Logout()
+
+	// Login
+	if err := c.Login("paul@cycle2u.com.au", "cappers11"); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Logged in")
+
+	// List mailboxes
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func () {
+		done <- c.List("", "*", mailboxes)
+	}()
+
+	log.Println("Mailboxes:")
+	for m := range mailboxes {
+		log.Println("* " + m.Name)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	// Select INBOX
+	mbox, err := c.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Flags for INBOX:", mbox.Flags)
+
+	// Get the last 4 messages
+	from := uint32(1)
+	to := mbox.Messages
+	if mbox.Messages > 3 {
+		// We're using unsigned integers here, only substract if the result is > 0
+		from = mbox.Messages - 3
+	}
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(from, to)
+
+	messages := make(chan *imap.Message, 10)
+	done = make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+
+	log.Println("Last 4 messages:")
+	for msg := range messages {
+		log.Println("* " + msg.Envelope.Subject)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Done!")
+}
+
+func main4() {
+	var (
+		from       = "bookings@cycle2u.com.au"
+		msg        = []byte("a test message")
+		recipients = []string{"paul@cycle2u.com.au"}
+	)
+
+	// hostname is used by PlainAuth to validate the TLS certificate.
+	hostname := "mail.cycle2u.com.au"
+	//auth := smtp.PlainAuth("", "paul@cycle2u.com.au", "cappers11", hostname)
+	//auth := smtp.CRAMMD5Auth("paul@cycle2u.com.au", "cappers11")
+
+	err := smtp.SendMail(hostname+":25", nil, from, recipients, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main3() {
+	// Connect to the remote SMTP server.
+	c, err := smtp.Dial("mail.cycle2u.com.au:25")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := c.Hello("mail.cycle2u.com.au"); err != nil {
+		log.Fatal(err)
+	}
+
+	//if err := c.Auth(smtp.CRAMMD5Auth("paul","cappers11")); err != nil {
+	//log.Fatal(err)
+	//}
+
+	if err := c.Auth(smtp.PlainAuth("", "paul", "cappers11", "mail.cycle2u.com.au")); err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the sender and recipient first
+	if err := c.Mail("bookings@cycle2u.com.au"); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Rcpt("paul@cycle2u.com.au"); err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the email body.
+	wc, err := c.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = fmt.Fprintf(wc, "Test email body")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = wc.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the QUIT command and close the connection.
+	err = c.Quit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main2() {
 	println("cycleserver")
 	m := &gomail.Message{}
 	m.SetFrom("Cycle2U Booking <bookings@cycle2u.com.au>")
