a1a4e4bf008080e9165e91835c03721e53fa2bef
steveoc <steveoc64@gmail.com>
2019-07-21 14:34:48 +1000

    getting it into a state where it can run independently

--- cycle2u/main.go
+++ cycle2u/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
-	"github.com/steveoc64/memdebug"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/steveoc64/memdebug"
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	memdebug.Print(time.Now(), r.RequestURI)
+	memdebug.Print(time.Now(), r.Method, r.RequestURI)
 	filename := r.RequestURI
-	if r.RequestURI == "/" {
+	if filename == "/" {
 		filename = "/index.html"
 	}
-	http.ServeFile(w,r,"static" + filename)
+	filename = "static" + filename
+	switch r.Method {
+	case "POST":
+		memdebug.Print(time.Now(), "Posting a booking")
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		println("name", r.FormValue("name"))
+		println("bike", r.FormValue("bike"))
+		println("equiry", r.FormValue("equiry"))
+		println("email", r.FormValue("email"))
+		println("telephone", r.FormValue("telephone"))
+		println("address", r.FormValue("address"))
+		println("message", r.FormValue("message"))
+		http.ServeFile(w, r, "static/thanks.html")
+	case "GET":
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			filename = "static/index.html"
+		}
+		http.ServeFile(w, r, filename)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", mainHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
